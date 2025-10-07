b754a80648a9d16a24fdeac3821ae57c29a4698e
Michail Vlassov <m.vlassov@singlequantum.com>
2025-10-07 12:24:50 +0200

    up

--- snspd_x1.cpp
+++ snspd_x1.cpp
@@ -8,7 +8,11 @@
 #include <vector>
 
 const double TSUBERROR = 1.01;
-const double MINRES = 0;
+const double GAMMA = 240;
+const double ALPHA_SF = 800;
+const double LORENTZ = 2.45e-8;
+const double G = 9.8;
+
 union uData
 {
    bool b;
@@ -29,6 +33,7 @@ union uData
 struct sSNSPD_X1
 {
     double resistance;
+    //double resistancePrev;
     double* temperatures;
     double  width;
     double  length;
@@ -51,16 +56,28 @@ struct sSNSPD_X1
     double* right_hand_side;
     double  Tmax;
     bool    running;
+    double prevCurrent;
+    double Lkin;
+    double V1;
+    double V2;
+    double prevR;
+    double starttime;
+    double Rmin;
+    int mod;
+    double A;
+    double y;
 
     // Constructor (optional, but good practice in C++)
-    sSNSPD_X1(union uData *data) : resistance(MINRES), temperatures(nullptr), time(0),
-    width(data[1].d), length(data[2].d), thickness(data[3].d), resolution(data[4].i), Tsub(data[6].d),
-    Tc(data[5].d),  Ic0K(data[7].d), Rsheet(data[8].d), hotspot(data[9].b), ths(data[10].d), 
-    Ths(data[11].d), sizehs(data[12].d), photonnumber(data[13].i){
+    sSNSPD_X1(union uData *data) : resistance(data[16].d), temperatures(nullptr), time(0),
+    width(data[2].d), length(data[3].d), thickness(data[4].d), resolution(data[5].i), Tsub(data[7].d),
+    Tc(data[6].d),  Ic0K(data[8].d), Rsheet(data[9].d), hotspot(data[10].b), ths(data[11].d), 
+    Ths(data[12].d), sizehs(data[13].d), photonnumber(data[14].i), Lkin(data[15].d), Rmin(data[16].d), mod(data[17].i){
         temperatures = new double[resolution]; // Use '->' to access member via pointer
         diagonal = new double[resolution];
         off_diagonal = new double[resolution];
         right_hand_side = new double[resolution];
+
+        A = 2.43*Tc*GAMMA;
 
         std::fill(temperatures, temperatures + resolution, Tsub);
         dlength    = length / (double)resolution;
@@ -87,10 +104,7 @@ int __stdcall DllMain(void *module, unsigned int reason, void *reserved) { retur
 #undef IN1
 #undef IN2
 #undef ROUT
-const double HEATCAP_ELECTRON_SF = 240;
-const double PHONON_HEATCAP_SF = 9.8;
-const double ALPHA_SF = 800;
-const double LORENTZ = 2.45e-8;
+
 
 
 // All Thermal functions
@@ -107,15 +121,19 @@ double calcKapaSC(double thickness, double Rsheet, double Tc, double temperature
 }
 
 double calcPhononHC(double temperature){
-    return PHONON_HEATCAP_SF * pow(temperature,3);
+    return G * pow(temperature,3);
 }
 
-double calcElectronHCSC(double Tc, double temperature){
-    return 2.43 * HEATCAP_ELECTRON_SF * Tc * exp(-2.15 * (pow(Tc,2) - pow(temperature,2))/(Tc * temperature));
+double calcPow( double Tc,  double T){
+    return -2.15*Tc*(1-pow(T/Tc,2))/T;
+}
+
+double calcElectronHCSC(double Tc, double temperature, double A){
+    return  A * exp(calcPow(Tc, temperature));
 }
 
 double calcElectronHC(double temperature){
-    return temperature * HEATCAP_ELECTRON_SF;
+    return temperature * GAMMA;
 }
 
 double calcIc(double Ic0K, double Tc, double temperature){
@@ -129,7 +147,7 @@ bool isSC(double current, double temperature, double Ic0K , double Tc){
 void createHotspot(sSNSPD_X1 *opaque, double current){
     int PNR = opaque->photonnumber;
     int hotspot_segments = (int)(opaque->sizehs * opaque->resolution / opaque->length);
-    double resistance = MINRES;
+    double resistance = opaque->Rmin;
     double Tmax = opaque->Tsub;
 
     for (int n = 0; n < PNR; ++n) {
@@ -149,7 +167,7 @@ void createHotspot(sSNSPD_X1 *opaque, double current){
 void diagonalSC(sSNSPD_X1 *opaque, int index, double dt, double* diagonal, double* off_diagonal, double* right_hand_side){
     double alpha = calcAlpha(opaque->temperatures[index]);
     double kapa = calcKapaSC(opaque->thickness, opaque->Rsheet, opaque->Tc, opaque->temperatures[index]);
-    double heat_cap = calcElectronHCSC(opaque->Tc, opaque->temperatures[index]) + calcPhononHC(opaque->temperatures[index]);
+    double heat_cap = calcElectronHCSC(opaque->Tc, opaque->temperatures[index], opaque->A) + calcPhononHC(opaque->temperatures[index]);
 
     double r = kapa * dt / (2 * pow(opaque->dlength,2) * heat_cap);
     double h = alpha * dt / (2 * opaque->thickness * heat_cap);
@@ -175,7 +193,7 @@ void  diagonalNSC(sSNSPD_X1 *opaque, int index, double dt, double current, doubl
 }
 
 void calcTotalResitance(sSNSPD_X1 *opaque, double current, double dt){
-    double resistance = MINRES;
+    double resistance = 0;
     double Tmax = opaque->Tsub;
 
     opaque->diagonal[0] = opaque->diagonal[opaque->resolution - 1] = 1.0;
@@ -194,51 +212,74 @@ void calcTotalResitance(sSNSPD_X1 *opaque, double current, double dt){
                       }
                   });
     double m;
+    double ai;
+    double ci1;
+    double* x;
+    x = new double[opaque->resolution];
+
     for (int it = 1; it < opaque->resolution; ++it) {
-        m = opaque->off_diagonal[it] / opaque->diagonal[it-1];
-        opaque->diagonal[it] = opaque->diagonal[it] - m * opaque->off_diagonal[it-1];
+        ai = opaque->off_diagonal[it];
+        ci1 = opaque->off_diagonal[it-1];
+        m = ai / opaque->diagonal[it-1];
+        opaque->diagonal[it] = opaque->diagonal[it] - m * ci1;
         opaque->right_hand_side[it] = opaque->right_hand_side[it] - m * opaque->right_hand_side[it-1];
     }
-    opaque->temperatures[opaque->resolution - 1] = opaque->right_hand_side[opaque->resolution - 1] / opaque->diagonal[opaque->resolution -1];
+    x[opaque->resolution - 1] = opaque->right_hand_side[opaque->resolution - 1] / opaque->diagonal[opaque->resolution -1];
     for (int il = opaque->resolution-2; il >=1; --il){
-        opaque->temperatures[il] = (opaque->right_hand_side[il] - opaque->off_diagonal[il] *  opaque->temperatures[il + 1]) / opaque->diagonal[il];
-        if (!isSC(current, opaque->temperatures[il], opaque->Ic0K, opaque->Tc)){
+        x[il] = (opaque->right_hand_side[il] - opaque->off_diagonal[il] *  x[il + 1]) / opaque->diagonal[il];
+        if (!isSC(current, x[il], opaque->Ic0K, opaque->Tc)){
             resistance += opaque->Rsegment;
         }
-        if (opaque->temperatures[il] > Tmax){
-            Tmax = opaque->temperatures[il];
-        }
     }
+    opaque->temperatures =x;
 
     opaque->resistance = resistance;
-    opaque->Tmax = Tmax;
 
+}
+
+double calcCurrentTrapezoidalRule(double V1, double V2, double V1prev, double V2prev, double Lkin, double dt, double R, double Rprev, double current){
+   double dV = (V1-V2);
+   double dVprev = (V1prev-V2prev);
+   double RL = 2.0*Lkin/dt;
+   return ( (RL-Rprev)* current + dV + dVprev)/(R+RL);
+}
+
+double calcBackwardsEuler(double V1, double V2,double Lkin, double dt, double R,double current){
+   double dV = (V1-V2);
+   double RL = Lkin/dt;
+   return (RL* current + dV)/(R+RL);
 }
 
 extern "C" __declspec(dllexport) void snspd_x1(struct sSNSPD_X1 **opaque, double t, union uData *data)
 {
-   double current = data[ 0].d; // input
-   double &ROUT = data[14].d; // output
-
+   double IN1          = data[ 0].d; // input
+   double IN2          = data[ 1].d; // input
+   double &ROUT = data[18].d; // output
+   //double &ROUTPREV = data[15].d; // output
    sSNSPD_X1 *inst = *opaque;
 
    if(!inst){
         inst = *opaque = new sSNSPD_X1(data);
-        ROUT=inst->resistance;
+        inst->starttime = t;
+   }
+
+   if (t==inst->starttime) {
+        ROUT = 0;
         inst->time = t;
         return;
    }
+
    double dt = t - inst->time;
    inst->time = t;
    if (inst->hotspot && t >= inst->ths){
         inst->hotspot = false;
-        createHotspot(inst, current);
+        createHotspot(inst, IN1);
    }
    else{
-    calcTotalResitance(inst, current, dt);
+    calcTotalResitance(inst, IN1, dt);
    }
-   ROUT=inst->resistance;
 
+   ROUT = inst->resistance;
 }
 
 extern "C" __declspec(dllexport) void Destroy(struct sSNSPD_X1 *inst)
